0c37bd18418e27b250bc03ef6a7ca58e858837ed
Arturas <arturas.petruskevicius@kiona.com>
2022-12-01 21:06:38 +0200

    Finish task

--- aoc/lib/mix.tasks/task_1.ex
+++ aoc/lib/mix.tasks/task_1.ex
@@ -5,19 +5,30 @@ defmodule Mix.Tasks.Task1 do
     Mix.Task.run("app.start")
 
     with stream <- File.stream!("1.txt") do
-      stream
-      |> Enum.map(&String.trim/1)
-      |> Enum.map(&String.to_integer/1)
-      |> IO.inspect()
-    end
+      snack_holdings =
+        stream
+        |> Enum.map(&String.trim/1)
+        |> Enum.chunk_by(&(&1 == ""))
+        |> Enum.filter(&(&1 != [""]))
+        |> Enum.map(
+          fn list -> Enum.map(
+            list,
+            &String.to_integer/1)
+          end
+        )
+        |> Enum.map(&Enum.sum/1)
 
-    # with {:ok, contents} <- File.read("1.txt") do
-    #   contents |> String.split("\r\n") |> IO.inspect()
-    #   # contents
-    #   # |> String.split("\n", trim: true)
-    #   # |> String.trim()
-    #   # |> IO.inspect()
-    # end
+      max_amount(snack_holdings) |> IO.inspect()
+      sum_of_n_largest_amounts(snack_holdings) |> IO.inspect()
+    end
   end
 
+  def max_amount(snack_holdings), do: snack_holdings |> Enum.max()
+
+  def sum_of_n_largest_amounts(snack_holdings) do
+    snack_holdings
+    |> Enum.sort()
+    |> Enum.take(-3)
+    |> Enum.sum()
+  end
 end
