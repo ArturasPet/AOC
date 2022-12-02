7c3c873cbbd4c80d9fa335f24b9d2f795052f3dc
Arturas <arturas.petrusk@iwmac.no>
2022-12-02 14:42:00 +0200

    Second part of second task

--- aoc/lib/mix.tasks/task_2.ex
+++ aoc/lib/mix.tasks/task_2.ex
@@ -28,6 +28,20 @@ defmodule Mix.Tasks.Task2 do
         )
         |> IO.inspect()
     end
+
+    with stream <- File.stream!("2.txt") do
+      snack_holdings =
+        stream
+        |> Enum.map(&String.trim/1)
+        |> Enum.reduce(
+             0,
+             fn round, acc ->
+               pair = String.split(round, " ")
+               acc = acc + round_result_scripted(pair)
+             end
+           )
+        |> IO.inspect()
+    end
   end
 
   def round_result(["A", "X"]), do: 3 + 1
@@ -39,4 +53,28 @@ defmodule Mix.Tasks.Task2 do
   def round_result(["C", "X"]), do: 6 + 1
   def round_result(["C", "Y"]), do: 0 + 2
   def round_result(["C", "Z"]), do: 3 + 3
+
+  def round_result_scripted(["A", goal]) do
+    case goal do
+      "X" -> 0 + 3
+      "Y" -> 3 + 1
+      "Z" -> 6 + 2
+    end
+  end
+
+  def round_result_scripted(["B", goal]) do
+    case goal do
+      "X" -> 0 + 1
+      "Y" -> 3 + 2
+      "Z" -> 6 + 3
+    end
+  end
+
+  def round_result_scripted(["C", goal]) do
+    case goal do
+      "X" -> 0 + 2
+      "Y" -> 3 + 3
+      "Z" -> 6 + 1
+    end
+  end
 end
