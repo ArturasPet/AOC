6d7bb30bb5bb0ce0a2b700d575178a3ac38933ea
Arturas <arturas.petrusk@iwmac.no>
2022-12-02 14:27:00 +0200

    First part of second task

---
+++ aoc/lib/mix.tasks/task_2.ex
@@ -0,0 +1,42 @@
+defmodule Mix.Tasks.Task2 do
+  use Mix.Task
+
+  def run(_) do
+    Mix.Task.run("app.start")
+
+    elves = %{
+        A: 1, # Goes against Y(Paper)
+        B: 2, # Goes against X(Rock)
+        C: 3  # Goes against Z(Scissors)
+      }
+    me = %{
+        X: 1,
+        Y: 2,
+        Z: 3
+      }
+
+    with stream <- File.stream!("2.txt") do
+      snack_holdings =
+        stream
+        |> Enum.map(&String.trim/1)
+        |> Enum.reduce(
+          0,
+          fn round, acc ->
+            pair = String.split(round, " ")
+            acc = acc + round_result(pair)
+          end
+        )
+        |> IO.inspect()
+    end
+  end
+
+  def round_result(["A", "X"]), do: 3 + 1
+  def round_result(["A", "Y"]), do: 6 + 2
+  def round_result(["A", "Z"]), do: 0 + 3
+  def round_result(["B", "X"]), do: 0 + 1
+  def round_result(["B", "Y"]), do: 3 + 2
+  def round_result(["B", "Z"]), do: 6 + 3
+  def round_result(["C", "X"]), do: 6 + 1
+  def round_result(["C", "Y"]), do: 0 + 2
+  def round_result(["C", "Z"]), do: 3 + 3
+end
